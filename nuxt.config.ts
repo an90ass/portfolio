bc421740e1b3cdbfd002d49c94d5cfccc1deef96
an90ass <anass12976@gmail.com>
2026-06-26 20:00:42 +0300

    feat: configure nuxt for github pages deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-01-01',
+compatibilityDate: '2025-01-01',
   devtools: { enabled: false },
+
+
+  ssr: true,
+  nitro: {
+    prerender: {
+      routes: ['/']
+    }
+  },
 
   modules: [
     '@nuxtjs/tailwindcss',
