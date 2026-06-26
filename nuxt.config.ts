72fe0e306e50a515bf9b820f3d04e10bb9f68617
an90ass <anass12976@gmail.com>
2026-06-26 20:03:03 +0300

    fix: set baseURL for github pages deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,10 @@
 export default defineNuxtConfig({
 compatibilityDate: '2025-01-01',
   devtools: { enabled: false },
-
+app: {
+    baseURL: '/portfolio/', 
+    buildAssetsDir: 'assets',
+  },
 
   ssr: true,
   nitro: {
